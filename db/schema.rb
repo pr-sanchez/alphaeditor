a4b544be28699e32101ecb5ca61482c346d472d3
Pedro Sánchez <pedrozo_96@hotmail.com>
2016-11-23 11:09:56 -0400

    Modificando mensaje de bienvenida y quien puede verlo o no

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161030175813) do
+ActiveRecord::Schema.define(version: 20161030203334) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
